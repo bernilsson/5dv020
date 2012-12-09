770398124c2f9a082e84d52de05afa0ff34daf37
Bertil Nilsson <bertilskola@gmail.com>
2012-12-09 15:26:52 +0100

    add IntegerSet, a class used for tracking sequences

--- build.sbt
+++ build.sbt
@@ -1,5 +1,14 @@
-name := "hello"
+name := "GCom"
 
 version := "1.0"
 
 scalaVersion := "2.9.2"
+
+resolvers += "Twitter" at "http://maven.twttr.com/"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.8" % "test"
+
+libraryDependencies += "org.scala-lang" % "scala-swing" % "2.9.2" 
+
+
+libraryDependencies += "com.twitter"   % "util-collection"   % "5.3.10"
---
+++ src/test/scala/com/gcom/test/testIntegerSet.scala
@@ -0,0 +1,29 @@
+package com.gcom.test
+
+import org.scalatest.FunSuite
+import com.gcom.IntegerSet
+
+class TestIntegerSet extends FunSuite{
+  test("IntegerSet should be empty"){
+    val set = new IntegerSet();
+    assert(set.ranges.length == 0);
+  }
+  test("Set should not contain duplicates"){
+    val set = new IntegerSet();
+    set.insert(1);
+    set.insert(1);
+    set.insert(1);
+    set.insert(1);
+    set.insert(1);
+    assert(set.contains(1) && set.ranges.length == 1);
+  }
+  test("Set should merge intervals"){
+    val set = new IntegerSet();
+    set.insert(1);
+    set.insert(2);
+    set.insert(5);
+    set.insert(4);
+    set.insert(3);
+    assert(set.ranges.length == 1)
+  }
+}
