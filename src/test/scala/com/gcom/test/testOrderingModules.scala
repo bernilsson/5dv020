2b8f79c5e67e1a5cafc1eca9bc2869cb8adb8051
bernilsson <bertilskola@gmail.com>
2012-12-26 14:39:52 +0100

    Add test for ordering module

---
+++ src/test/scala/com/gcom/test/testOrderingModules.scala
@@ -0,0 +1,34 @@
+package com.gcom.test
+import org.scalatest._
+import com.gcom.UnorderedQueue
+import com.gcom.IM
+import com.gcom.DM
+import com.gcom.Message
+import com.gcom.SimpleMessage
+import com.gcom.Node
+import com.gcom.SeqM
+import com.gcom.DataMessage
+import com.gcom.FIFOQueue
+import scala.util.Random
+
+
+class testOrderingModules extends FunSuite {
+  def createSimpleMessage(msg: DataMessage[String]) = IM( SimpleMessage(Node("f", 0, "n")), msg) 
+  //def createMessageFrom()
+  
+  test("Unordered Queue should hold messages"){
+    val q = new UnorderedQueue[String]();
+    for(i <- 1 to 10 ) q.insert(createSimpleMessage(DM("A Long Data" + i)));
+    assert(q.getAll.length === 10)
+  }
+  
+  def createFIFOMsg(seq: Int, value: String) = createSimpleMessage(SeqM(seq,value))
+  
+  test("FIFOQueue should deliver messages in correct order"){
+    val q = new FIFOQueue[String]();
+    val rand = new Random();
+    for(i <- rand.shuffle(Range(0,10))) q insert createFIFOMsg(i, "l" + i);
+    assert(q.getAll.toSeq === Range(0,10).map("l" + _))
+  }
+  
+}
