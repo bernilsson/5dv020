ae13715f1f4598c46844698ceeeee1528ebab654
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2013-01-11 02:59:27 +0100

    Simple option parsing with Argot.

--- client/build.sbt
+++ client/build.sbt
@@ -19,3 +19,5 @@ libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.0.7"
 seq(com.github.retronym.SbtOneJar.oneJarSettings: _*)
 
 libraryDependencies += "commons-lang" % "commons-lang" % "2.6"
+
+libraryDependencies += "org.clapper" % "argot_2.10" % "1.0.0"
--- nameserver/src/NameServer.scala
+++ nameserver/src/NameServer.scala
@@ -1,2 +1,26 @@
+package gcom.nameserver
+
 object NameServer {
-  def main(args: Array[String]) = println("nameserver:main")}
+
+  import org.clapper.argot.ArgotParser
+  import org.clapper.argot.ArgotUsageException
+  import org.clapper.argot.ArgotConverters._
+
+  val parser = new ArgotParser(
+    "gcom-nameserver",
+    preUsage=Some("gcom-nameserver 0.1")
+  )
+
+  val portopt = parser.option[Int](List("p", "port"), "n", "port number")
+  lazy val port = portopt.value.getOrElse(31337)
+
+  def main(args: Array[String]) = {
+    try {
+      parser.parse(args)
+      println("Running nameserver on port " + port)
+    }
+    catch {
+      case e : ArgotUsageException => println(e.message);
+    }
+  }
+}
