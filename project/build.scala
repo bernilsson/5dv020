1fc07916769c37ad593dbb969118e7b513c74584
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2013-01-12 22:53:09 +0100

    More deployment stuff.

--- project/build.scala
+++ project/build.scala
@@ -1,16 +1,35 @@
+import com.github.retronym.SbtOneJar
 import sbt._
 import Keys._
 
 object GComBuild extends Build {
+    def standardSettings = Seq(
+      exportJars := true
+    ) ++ Defaults.defaultSettings
+
     lazy val root = Project(id = "lab2",
-                            base = file(".")) aggregate(gcom, nameserver, client)
+                            base = file("."),
+                            aggregate = Seq(gcom, registry, nameserver, client))
 
     lazy val gcom = Project(id = "gcom",
-                            base = file("gcom"))
+                            base = file("gcom"),
+                            settings = standardSettings)
+
+    lazy val registry = Project(id = "registry",
+                                base = file("registry"),
+                                dependencies = Seq(gcom),
+                                settings = standardSettings ++
+                                  SbtOneJar.oneJarSettings)
 
     lazy val nameserver = Project(id = "nameserver",
-                                  base = file("nameserver")) dependsOn(gcom)
+                                  base = file("nameserver"),
+                                  dependencies = Seq(gcom),
+                                  settings = standardSettings ++
+                                    SbtOneJar.oneJarSettings)
 
     lazy val client = Project(id = "client",
-                              base = file("client")) dependsOn(gcom)
+                              base = file("client"),
+                              dependencies = Seq(gcom),
+                              settings = standardSettings ++
+                                SbtOneJar.oneJarSettings)
 }
