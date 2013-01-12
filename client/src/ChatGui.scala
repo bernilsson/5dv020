3095b91a3be6a590f9533e836eb1e7424af53256
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2013-01-12 22:19:27 +0100

    Trailing whitespace, 80-col violations.

--- client/src/ChatGui.scala
+++ client/src/ChatGui.scala
@@ -1,4 +1,3 @@
-
 import swing._
 import swing.event._
 import java.awt.FlowLayout
@@ -20,41 +19,42 @@ object DummyNameServer{
         }
       }
     }).start();
-    
+
     new DummyCommunicator(onRecv);
   }
-  def listGroups(): List[Group] = { 
-    List(Group("Group1", Reliable(),FIFO()), Group("Group2", NonReliable(),Total()));
+  def listGroups(): List[Group] = {
+    List(Group("Group1", Reliable(),FIFO()),
+         Group("Group2", NonReliable(),Total()));
   }
 }
 
 object ChatGui extends SimpleSwingApplication {
-  
+
   //initialize NameServer
- 
-  
+
+
   def top = new MainFrame {
     title = "ChatGui"
-      
+
     import Dialog._
     val possibilities = DummyNameServer.listGroups;
     val buttons = new BoxPanel(Orientation.Vertical) {
-        
+
       }
     val s = showInput[Group](buttons,
       "Select a server",
       "Server Selection",
-      Message.Question, 
+      Message.Question,
       Swing.EmptyIcon,
       possibilities, null)
-   
+
     if(s.isEmpty){
       quit()
-    }  
-      
+    }
+
     val com = DummyNameServer.joinGroup(
-        ExistingGroup(s.get.groupName), 
-        { msg => 
+        ExistingGroup(s.get.groupName),
+        { msg =>
           chatBox.append("\n" + msg)
           javax.swing.SwingUtilities.invokeLater(new Runnable() {
                   def run() {
@@ -78,20 +78,20 @@ object ChatGui extends SimpleSwingApplication {
     object chatInput extends TextField(30){
       horizontalAlignment = Alignment.Left;
     };
-    
+
     object nodeList extends ListView(List("Waiting for group info"))
-    
+
     object inputPanel extends FlowPanel{
       contents.append(chatInput, button);
     }
-    
+
     contents = new BoxPanel(Orientation.Vertical) {
       contents.append(new BoxPanel(Orientation.Horizontal){
-       contents.append(chatScroller, new ScrollPane(nodeList)) 
+       contents.append(chatScroller, new ScrollPane(nodeList))
       }, inputPanel)
       border = Swing.EmptyBorder(5, 5, 5, 5)
     }
-    
+
     listenTo(button)
     listenTo(chatInput)
     reactions += {
@@ -100,7 +100,7 @@ object ChatGui extends SimpleSwingApplication {
       case EditDone(_) =>
         com.broadCastMsg(chatInput.text)
     }
-      
+
     override def closeOperation(){
       com.leaveGroup
       super.closeOperation
@@ -108,4 +108,3 @@ object ChatGui extends SimpleSwingApplication {
   }
 
 }
-
