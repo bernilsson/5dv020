d62baf397533ad8fce175deaa54a57b623f15b1d
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2013-01-12 22:19:27 +0100

    Trailing whitespace, 80-col violations.

--- client/src/DebugGUI.scala
+++ client/src/DebugGUI.scala
@@ -24,25 +24,26 @@ object DummyNameServer{
         }
       }
     }).start();
-    
+
     new DummyCommunicator(onRecv);
   }
   def listGroups(): List[Group] = {
-    List(Group("Group1", Reliable(),FIFO()), Group("Group2", NonReliable(),Total()));
+    List(Group("Group1", Reliable(),FIFO()),
+    * Group("Group2", NonReliable(),Total()));
   }
 }
 */
 object DebugGui extends SimpleSwingApplication {
-  
+
   //initialize NameServer
   UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName)
-  
+
   var messageQueues = Map[String,List[Message]]();
-  
+
   def top = new MainFrame {
     title = "ChatGui"
-    
-    /* Display a dialog for user to select group before showing gui */  
+
+    /* Display a dialog for user to select group before showing gui */
     import Dialog._
     val possibilities = DummyNameServer.listGroups;
     val buttons = new BoxPanel(Orientation.Vertical)
@@ -52,15 +53,15 @@ object DebugGui extends SimpleSwingApplication {
       Message.Plain,
       Swing.EmptyIcon,
       possibilities, null)
-   
+
     if(s.isEmpty){
       quit()
     }
-      
+
     val com = DummyNameServer.joinGroup(
         ExistingGroup(s.get.groupName),
         { msg =>
-          
+
           chatBox.append("\n" + msg)
           /* Work around bug where maximum was not yet updated, let swing
            * handle scrolling down.
@@ -73,23 +74,23 @@ object DebugGui extends SimpleSwingApplication {
               }
           );
          });
-    
+
     object dropText extends Label("Delay: ")
     object dropInput extends CheckBox
     object delayText extends Label("Drop: ")
     object delayInput extends TextField{
       columns = 3;
     }
-    
+
     object queueList extends ListView[String]{
       listData = List("No queues...")
     }
-    
+
     object messageList extends ListView[String]{
       listData = List("Messages are to be displayed here")
     }
-    
-    
+
+
     object button extends Button {
       text = "Send"
     }
@@ -107,12 +108,12 @@ object DebugGui extends SimpleSwingApplication {
          val delay = try{
             delayInput.text.toInt
             } catch{
-              case e: NumberFormatException => 
+              case e: NumberFormatException =>
                 0
             }
           com.broadCastMsg(chatInput.text)
       }
-      
+
     }
 
     object scrollingChatBox extends ScrollPane{
@@ -121,15 +122,15 @@ object DebugGui extends SimpleSwingApplication {
     object nodeList extends ListView[String]{
       listData = List("Waiting for node info...")
     }
-    
+
     object counter extends Label("0 msg/sec")
-     
-    
+
+
     object chatInput extends TextField{
       preferredSize = new Dimension(250,30)
       horizontalAlignment = Alignment.Left;
     };
-    
+
     object inputPanel extends FlowPanel{
       //For some reason objects are not displayed in the same order as added
       contents.append(counter,
@@ -137,10 +138,10 @@ object DebugGui extends SimpleSwingApplication {
           delayText,
           dropInput,
           dropText,
-          delayInput, 
+          delayInput,
           button);
     }
-    
+
     contents = new GridBagPanel {
       import java.awt.GridBagConstraints._;
       val chatAndGroupMembers = new GridBagPanel {
@@ -161,7 +162,7 @@ object DebugGui extends SimpleSwingApplication {
           weighty = 1
           anchor = GridBagPanel.Anchor.LineStart
           fill = GridBagPanel.Fill.Both
-          
+
         })
       }
       //contents.append(scroller, inputPanel)
@@ -169,11 +170,13 @@ object DebugGui extends SimpleSwingApplication {
       addComponent(new ScrollPane(messageList),1, 0, 1, 1, 1 ,1, NORTH,  BOTH)
       addComponent(chatAndGroupMembers,        0, 1, 2, 1, 1 , 1, NORTH, BOTH)
       addComponent(inputPanel,                 0, 2, 2, 1, 1 , 0, NORTH, BOTH)
-      
+
       border = Swing.EmptyBorder(5, 5, 5, 5)
 
-      def addComponent(component: Component, x: Int, y: Int, width: Int, height: Int,
-         weightx: Double, weighty: Double, anchor: Int, fill: Int): Unit = {
+      def addComponent(component: Component, x: Int, y: Int,
+                       width: Int, height: Int,
+                       weightx: Double, weighty: Double,
+                       anchor: Int, fill: Int): Unit = {
       val gbc = new GridBagConstraints
       gbc.gridx = x
       gbc.gridy = y
@@ -185,12 +188,12 @@ object DebugGui extends SimpleSwingApplication {
       gbc.fill = fill
       add(component,new Constraints(gbc))
     }
-    
+
     }
-    
+
     case class UpdateQueue(name: String,list: List[Message]) extends Event
     case class UpdateSentMessages(num: Double) extends Event
-    
+
     listenTo(queueList.selection)
     reactions += {
       case UpdateQueue(name, list) => {
@@ -198,7 +201,7 @@ object DebugGui extends SimpleSwingApplication {
         messageQueues += (name -> list)
         updateList();
         queueList.selectIndices(selection)
-        
+
       }
       // `` lets us match against that specific variable.
       case ListSelectionChanged(`queueList`,range,live) => {
@@ -209,11 +212,11 @@ object DebugGui extends SimpleSwingApplication {
         counter.text = num.toString + " m/sec "
       }
     }
-    
+
     private def updateList() = {
       queueList.listData_=(messageQueues.keySet.toSeq)
     }
-    
+
     override def closeOperation(){
       com.leaveGroup
       super.closeOperation
@@ -221,4 +224,3 @@ object DebugGui extends SimpleSwingApplication {
   }
 
 }
-
