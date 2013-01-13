d3c67e8dc414d32c418ae6dd45b6757eb216b083
Bertil Nilsson <bertilskola@gmail.com>
2013-01-13 11:27:37 +0100

    WIP

--- gcom/src/ordering/CausalTotal.scala
+++ gcom/src/ordering/CausalTotal.scala
@@ -1,1 +1,14 @@
 package gcom.ordering;
+
+import gcom.communication.Communication
+import gcom.common.Message
+import gcom.common.NodeID
+import gcom.common.MessageOrdering
+
+class CausalTotal(c: Communication, callbck : Message => Unit, me: NodeID , nextOrder : () => Int) extends Ordering (c: Communication, callbck : Message => Unit) {
+  def receiveMessage(msg : Message) : Unit
+  def sendToAll(dst: List[NodeID],payload: String) : Unit = {
+    communicator.sendToAll(dst, payload,createOrdering())
+  }
+  protected def createOrdering() : MessageOrdering
+}
