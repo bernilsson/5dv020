1d7bec5dead0ef4ccf60938a81898d818c8fe4d9
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2013-01-14 02:14:51 +0100

    -p option for the registry.

--- registry/build.sbt
+++ registry/build.sbt
@@ -4,6 +4,8 @@ scalaVersion := "2.10.0"
 
 scalacOptions += "-deprecation"
 
+libraryDependencies += "org.clapper" % "argot_2.10" % "1.0.0"
+
 scalaSource in Compile <<= baseDirectory(_ / "src")
 
 scalaSource in Test <<= baseDirectory(_ / "test")
--- registry/src/Registry.scala
+++ registry/src/Registry.scala
@@ -4,12 +4,26 @@ import java.rmi.registry.LocateRegistry
 /* We run our own registry so that CLASSPATH is always correct. */
 object GComRegistry {
 
+  /* Option parsing. */
+  import org.clapper.argot.ArgotParser
+  import org.clapper.argot.ArgotUsageException
+  import org.clapper.argot.ArgotConverters._
+
+  val parser = new ArgotParser(
+    "gcom-nameserver",
+    preUsage=Some("gcom-nameserver 0.1")
+  )
+
+  /* Option values. */
+  val portopt = parser.option[Int](List("p", "port"), "n", "port number")
+  lazy val port = portopt.value.getOrElse(31337)
+
+
   def main(args : Array[String]) {
     if (System.getSecurityManager == null) {
       System.setSecurityManager(new SecurityManager)
     }
 
-    val port = 31337
     LocateRegistry.createRegistry(port)
     println("Running GCom registry on port " + port.toString)
     readLine()
