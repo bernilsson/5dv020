302bd8dc982fa619d879f71f7e2f39414ccf2943
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2013-01-15 02:38:27 +0100

    Working group management, with tests.

--- gcom/src/group/Group.scala
+++ gcom/src/group/Group.scala
@@ -1,27 +1,34 @@
 package gcom.group;
 
+import java.rmi.Remote
+import java.rmi.RemoteException
+import java.rmi.registry.LocateRegistry
+import java.rmi.registry.Registry
+import java.rmi.server.UnicastRemoteObject
+
 import org.slf4j.Logger
+
+import scala.collection.mutable.Stack
+import scala.collection.concurrent.TrieMap
 
 import gcom.NameServer
 import gcom.Group
 import gcom.common._
 import gcom.communication.Communication
 import gcom.ordering.Ordering
-import gcom.transport.BasicTransport
 import gcom.Communicator
 
 // Dummy group: Stores all data on the name server.
 
 class DummyGroup (grp : Group, lggr : Logger, nsrv : NameServer,
                   ndID : NodeID, ord : Ordering,
-                  callbck : String => Unit, viewCllbck : Set[NodeID] => Unit)
+                  callbck : String => Unit)
       extends Communicator {
   val group        = grp
   val nameserver   = nsrv
   val ordering     = ord
   val nodeID       = ndID
   var callback     = callbck
-  var viewCallback = viewCllbck
   val logger       = lggr
 
   def incCounter() : Int = { nsrv.incCounter() }
@@ -48,15 +55,368 @@ class DummyGroup (grp : Group, lggr : Logger, nsrv : NameServer,
 object DummyGroup {
   def create(grp : Group, lggr : Logger, nsrv : NameServer, ndID: NodeID,
              comm : Communication, ord : Ordering,
-             callbck : String => Unit, viewCallbck : Set[NodeID] => Unit)
+             callbck : String => Unit)
               : Communicator = {
     nsrv.getOrSetGroupLeader(grp, ndID)
     val dummy = new DummyGroup(grp, lggr, nsrv, ndID, ord,
-                               callbck, viewCallbck)
+                               callbck)
     comm.setHostCallback(dummy.listGroupMembers)
     ord.setOnReceive(dummy.receiveMessage)
     ord.setOrderCallback(dummy.incCounter)
     nsrv.joinGroup(ndID)
     return dummy
+  }
+}
+
+/* Non-dummy group: uses 2PC to agree on a sequence of commands. */
+
+// Group state.
+sealed case class GroupState
+(leader : NodeID, members : Map[NodeID, Int],
+ msgCounter : Int, // counter for the total order
+ opCounter : Int,  // # of the last agreed-upon GroupStateOp
+ nodeCounter : Int) // # of the last joined node
+
+/* Group state ops. */
+sealed abstract class GroupStateOp
+case class JoinGroup(node : NodeID) extends GroupStateOp;
+case class LeaveGroup(nodes : Set[NodeID]) extends GroupStateOp;
+case class IncCounter() extends GroupStateOp;
+case class NewLeader(n : NodeID) extends GroupStateOp;
+
+// Remote interface for 2PC. Not using the communication layer for this because
+// to avoid delayed/dropped msgs. We pretend that a reliable protocol (e.g. TCP)
+// is used for consensus and an unreliable one (e.g. UDP) for passing messages
+// around.
+trait GroupCommunication extends Remote {
+  // Called by a new node that wants to join.
+  @throws(classOf[RemoteException])
+  def letMeIn(who : NodeID) : Option[GroupState]
+
+  // 2PC: prepare.
+  @throws(classOf[RemoteException])
+  def consensusPrepare(from : NodeID, n: Int, op : GroupStateOp) : Boolean
+
+  // 2PC: commit.
+  @throws(classOf[RemoteException])
+  def consensusCommit(from : NodeID, n: Int, op : GroupStateOp) : Unit
+
+  // 2PC: abort.
+  @throws(classOf[RemoteException])
+  def consensusAbort(from : NodeID, sn: Int, op : GroupStateOp) : Unit
+
+  @throws(classOf[RemoteException])
+  def ping() : Unit;
+}
+
+class BasicGroup (grp: Group, lggr : Logger, nsrv : NameServer,
+                  ndID : NodeID, ord : Ordering,
+                  callbck : String => Unit)
+      extends GroupCommunication with Communicator {
+  val group        = grp
+  val nameserver   = nsrv
+  val ordering     = ord
+  val nodeID       = ndID
+  var callback     = callbck
+  val logger       = lggr
+
+  @volatile
+  var state : GroupState = null
+  var history            = Stack[(Int, GroupStateOp)]()
+
+  // Copy-paste from Transport. TOFIX: remove duplication.
+  val registries = new TrieMap[(String, Int), Registry]();
+  val stubs      = new TrieMap[NodeID, GroupCommunication]();
+
+  /* If we can't locate registry, dying with an exception is fine. */
+  private def locateRegistry(host : String, port : Int) : Registry = {
+    registries.get((host, port)) match {
+      case None => { val registry = LocateRegistry.getRegistry(host, port);
+                     registries((host, port)) = registry;
+                     return registry;
+                   }
+      case Some(r) => r
+    }
+  }
+
+  /* It's OK to throw exceptions here: can't locate stub - we're toast. */
+  private def locateStub(n : NodeID) : Option[GroupCommunication] = {
+    stubs.get(n) match {
+      case None => {
+        val registry = locateRegistry(n.host, n.port)
+        val stub = registry.lookup(n.name + "-group")
+                   .asInstanceOf[GroupCommunication]
+        stubs(n) = stub
+        Some(stub)
+      }
+      case s    => s
+    }
+  }
+
+  private def register() = {
+    val stub = UnicastRemoteObject.exportObject(this, 0)
+    val registry = locateRegistry(nodeID.host, nodeID.port)
+    registry.rebind(nodeID.name + "-group", stub)
+    logger.debug("Group communication ready")
+  }
+
+  def ping : Unit = { ; }
+
+  def pingNode(dst : NodeID) : Boolean = {
+    try {
+      val mstub = locateStub(dst)
+      mstub.map { stub =>
+        stub.ping(); true;
+      }.getOrElse(false)
+    }
+    catch {
+      case _ : RemoteException => false
+    }
+  }
+
+  // Initialisation.
+  def joinGroup() : Unit = {
+    val leader = nsrv.getOrSetGroupLeader(grp, ndID)
+
+    if (leader == nodeID) {
+      state = new GroupState(nodeID, Map(nodeID -> 0), 0, 0, 0)
+    }
+    else {
+      val stub = locateStub(leader).get
+      stub.letMeIn(nodeID) match {
+        case None => {
+          logger.debug("They don't let me in! (shouldn't happen)")
+          assert(false)
+        }
+        case Some(s) => {
+          logger.debug("They let me in! Praise the gods!")
+          state = s
+        }
+      }
+    }
+  }
+
+  // Let a new node join the group.
+  def letMeIn(who : NodeID) : Option[GroupState] = {
+    if (state.members.contains(who)) {
+      return None;
+    }
+    else {
+      return updateSharedState(JoinGroup(who))
+    }
+  }
+
+  /* 2PC impl. */
+  var next2PCAction : Option[(NodeID, Int, GroupStateOp)] = None
+
+  // 2PC: prepare.
+  def consensusPrepare(from : NodeID, num : Int, op : GroupStateOp) : Boolean = {
+    next2PCAction match {
+      case Some(t@(frm, n, o)) => {
+        if (t == (from, num, op)) true
+        else { if (!pingNode(frm)) { next2PCAction = Some(from, num, op); true}
+               else { false } }
+      }
+      case None => { next2PCAction = Some(from, num, op); true }
+    }
+  }
+
+  // 2PC: commit.
+  def consensusCommit(from : NodeID, num : Int, op : GroupStateOp) : Unit = {
+    next2PCAction match {
+      case Some(t@(frm, n, o)) => {
+        if (t == (from, num, op)) {
+          performGroupStateOp(num, op)
+          next2PCAction = None
+        }
+        else  {
+          logger.debug("consensusCommit: Asked to commit a wrong op.")
+          assert(false)
+        }
+      }
+      case None => {
+        logger.debug("consensusCommit: commit w/o prepare.")
+        assert(false)
+      }
+    }
+  }
+
+  // 2PC: abort.
+  def consensusAbort(from : NodeID, num : Int, op : GroupStateOp) : Unit = {
+    next2PCAction match {
+      case Some(t@(frm, n, o)) => {
+        if (t == (from, num, op)) { next2PCAction = None }
+        else { ; }
+      }
+      case None => { ; }
+    }
+  }
+
+  // Consensus.
+
+  private def updateSharedState(op : GroupStateOp) : Option[GroupState] = {
+    import scala.util.control.Breaks._
+
+    var prepared = false
+    var committed = false
+    while (!committed) {
+      val newOpCounter = state.opCounter + 1
+      val groupMembers = state.members.keySet
+
+      breakable {
+        for (member <- groupMembers) {
+          val mstub = locateStub(member)
+          mstub match {
+            case Some(stub) => {
+              prepared = stub.consensusPrepare(nodeID, newOpCounter, op)
+              if (!prepared)
+                break;
+            }
+            case None =>
+              logger.debug("updateSharedState: node " + member + "missing.")
+          }
+        }
+      }
+      for (member <- groupMembers) {
+        val mstub = locateStub(member)
+        mstub match {
+          case Some(stub) => {
+            if(prepared)
+              stub.consensusCommit(nodeID, newOpCounter, op)
+            else
+              stub.consensusAbort(nodeID, newOpCounter, op)
+          }
+          case None =>
+            logger.debug("updateSharedState: node " + member + "missing.")
+        }
+      }
+      if (prepared) committed = true
+      if (!committed) Thread.sleep(500) // hacky
+    }
+
+    return Some(state)
+  }
+
+  private def electNewLeader(notMe : Boolean) : Unit = {
+    try {
+      var done = false
+      while (!state.members.isEmpty && !done) {
+        val membersFiltered = if (notMe) { state.members.filter(_._1 != nodeID) }
+                              else { state.members }
+        if (!membersFiltered.isEmpty) {
+          val candidateLeader = membersFiltered.minBy(_._2)._1
+          if (pingNode(candidateLeader)) {
+            updateSharedState(NewLeader(candidateLeader))
+            nameserver.setGroupLeader(group, candidateLeader)
+            done = true
+          }
+          else {
+            updateSharedState(LeaveGroup(Set(candidateLeader)))
+          }
+        }
+        else {
+          // Group empty, we're shutting down.
+          nameserver.removeGroup(group)
+          done = true
+        }
+      }
+    }
+    catch {
+      // Can't reach the nameserver: network failure, we're toast.
+      case e : RemoteException => killGroup()
+    }
+  }
+
+  // Called after consensus has been reached.
+  private def performGroupStateOp(num : Int, op : GroupStateOp) {
+    state.synchronized {
+    logger.debug("performGroupStateOp( " + num.toString
+                 + ", " + op.toString + " )")
+    op match {
+      case JoinGroup(node) =>
+        { val newNodeCounter = state.nodeCounter + 1
+          state = state.copy(members = state.members + (node -> newNodeCounter),
+                             nodeCounter = newNodeCounter);
+        }
+      case LeaveGroup(nodes) =>
+        { state = state.copy(members = state.members -- nodes);
+        }
+      case IncCounter() => state = state.copy(msgCounter = state.msgCounter + 1);
+      case NewLeader(n) => state = state.copy(leader = n);
+    }
+
+    state = state.copy(opCounter = state.opCounter + 1)
+    assert(state.opCounter == num)
+    history.push((num, op))
+
+    op match {
+      case LeaveGroup(s) => {
+        // TODO: Update UI
+        if (s.contains(nodeID)) {
+          state = null
+          // TODO: Kill myself
+        }
+      }
+      case JoinGroup(n) => {
+        // TODO: Update UI
+      }
+      case _ => ;
+    }
+    }
+  }
+
+  // Required ops.
+  def incCounter() : Int = {
+    val newState = updateSharedState(IncCounter())
+    assert(!newState.isEmpty)
+    return state.msgCounter
+  }
+  def broadcastMessage(msg : String) : Unit = {
+    val membersSet = this.listGroupMembers()
+    val reachable = ordering.sendToAll(membersSet, msg)
+    val unreachable = membersSet.diff(reachable);
+    updateSharedState(LeaveGroup(unreachable))
+    if (unreachable.contains(state.leader)) {
+      electNewLeader(false)
+    }
+  }
+
+  def listGroupMembers() : Set[NodeID] = state.members.keySet
+  def leaveGroup() : Unit = {
+    val iWasLeader = if (state.leader == nodeID) true else false
+    if (iWasLeader)
+      electNewLeader(true)
+    updateSharedState(LeaveGroup(Set(nodeID)));
+    state = null
+  }
+  def killGroup() : Unit = {
+    nameserver.removeGroup(group);
+    updateSharedState(LeaveGroup(this.listGroupMembers()))
+    state = null
+  }
+
+  // Boilerplate.
+  def setOnReceive(callbck : String => Unit) = { callback = callbck; }
+  def receiveMessage(msg : Message) = {
+    msg match {
+      case Message(_, _, payload) => callback(payload)
+    }
+  }
+}
+
+object BasicGroup {
+  def create(grp : Group, lggr : Logger, nsrv : NameServer, ndID: NodeID,
+             comm : Communication, ord : Ordering,
+             callbck : String => Unit)
+            : BasicGroup = {
+    val ret = new BasicGroup(grp, lggr, nsrv, ndID, ord, callbck)
+    ret.register()
+    ret.joinGroup()
+
+    comm.setHostCallback(ret.listGroupMembers)
+    ord.setOnReceive(ret.receiveMessage)
+    ord.setOrderCallback(ret.incCounter)
+
+    return ret
   }
 }
---
+++ gcom/test/group/GroupTest.scala
@@ -0,0 +1,248 @@
+import java.rmi.registry.Registry
+import java.rmi.registry.LocateRegistry
+
+import org.scalatest._
+import org.slf4j.LoggerFactory
+
+import gcom._
+import gcom.common._
+import gcom.transport._
+import gcom.communication._
+import gcom.ordering._
+import gcom.group._
+
+/** Requires an rmiregistry and nameserver running on port 31337. */
+class GroupSpec extends FlatSpec with BeforeAndAfter {
+  behavior of "the group management module"
+
+  val host       = Util.getLocalHostName
+  val port       = 31337
+  val nameserver = connectToNameserver(NodeID("nameserver", host, port))
+  val logger     = LoggerFactory.getLogger("groupspec")
+  val group      = Group("default", UnreliableMulticast(), NoOrdering())
+
+  // Dummies, not used
+  val transport     = BasicTransport.create(NodeID("dummy", host, port),
+                                            {msg => }, logger);
+  val communication = NonReliable.create(transport, {msg =>})
+  val ordering      = NonOrdered.create(communication, {msg => })
+
+  // Helpers
+  def connectToNameserver(nameserver : NodeID) : NameServer = {
+    val registry = LocateRegistry.getRegistry(nameserver.host, nameserver.port)
+    val stub = registry.lookup(nameserver.name).asInstanceOf[NameServer]
+
+    return stub
+  }
+
+  before {
+    nameserver.clearGroups()
+  }
+
+  after {
+    nameserver.clearGroups()
+  }
+
+  "Empty name server" should "be running" in {
+    val grps = nameserver.listGroups()
+    assert(grps.isEmpty)
+  }
+
+  "Single nodes" should "be able to create singleton groups" in {
+    val nodeID = new NodeID("node1", host, port)
+    val communicator =
+      BasicGroup.create(group, logger, nameserver,
+                        nodeID, communication, ordering, {msg =>})
+
+    assert(communicator.state.leader === nodeID)
+    assert(communicator.state.members.size === 1)
+  }
+
+  "Nodes" should "be able to join groups" in {
+    val node1 = new NodeID("node1", host, port)
+    val node2 = new NodeID("node2", host, port)
+    val communicator1 =
+      BasicGroup.create(group, logger, nameserver,
+                        node1, communication, ordering, {msg =>})
+    val communicator2 =
+      BasicGroup.create(group, logger, nameserver,
+                        node2, communication, ordering, {msg =>})
+
+    assert(communicator1.state.leader === node1)
+    assert(communicator2.state.leader === node1)
+    assert(communicator1.state.members.size === 2)
+    assert(communicator2.state.members.size === 2)
+    assert(communicator1.state.members === communicator2.state.members)
+    assert(communicator1.state.members === Map(node1 -> 0, node2 -> 1))
+
+    val node3 = new NodeID("node3", host, port)
+    val communicator3 =
+      BasicGroup.create(group, logger, nameserver,
+                        node3, communication, ordering, {msg =>})
+
+    assert(communicator3.state.leader === node1)
+    assert(communicator1.state.members.size === 3)
+    assert(communicator2.state.members.size === 3)
+    assert(communicator3.state.members.size === 3)
+    assert(communicator1.state.members === communicator2.state.members)
+    assert(communicator3.state.members === Map(node1 -> 0, node2 -> 1,
+                                               node3 -> 2))
+  }
+
+  "Nodes" should "be able to leave groups" in {
+    val node1 = new NodeID("node1", host, port)
+    val node2 = new NodeID("node2", host, port)
+    val node3 = new NodeID("node3", host, port)
+    val communicator1 =
+      BasicGroup.create(group, logger, nameserver,
+                        node1, communication, ordering, {msg =>})
+    val communicator2 =
+      BasicGroup.create(group, logger, nameserver,
+                        node2, communication, ordering, {msg =>})
+    val communicator3 =
+      BasicGroup.create(group, logger, nameserver,
+                        node3, communication, ordering, {msg =>})
+
+    assert(communicator1.state.members === communicator2.state.members)
+    assert(communicator3.state.members === Map(node1 -> 0, node2 -> 1,
+                                               node3 -> 2))
+
+    communicator3.leaveGroup()
+    assert(communicator1.state.members === communicator2.state.members)
+    assert(communicator2.state.members === Map(node1 -> 0, node2 -> 1))
+
+    communicator2.leaveGroup()
+    assert(communicator1.state.members === Map(node1 -> 0))
+
+    // communicator1.leaveGroup()
+    // assert(communicator1.state === null)
+    // assert(nameserver.listGroups.isEmpty)
+  }
+
+  "Leaders" should "be able to leave groups" in {
+    val node1 = new NodeID("node1", host, port)
+    val node2 = new NodeID("node2", host, port)
+    val node3 = new NodeID("node3", host, port)
+    val communicator1 =
+      BasicGroup.create(group, logger, nameserver,
+                        node1, communication, ordering, {msg =>})
+    val communicator2 =
+      BasicGroup.create(group, logger, nameserver,
+                        node2, communication, ordering, {msg =>})
+    val communicator3 =
+      BasicGroup.create(group, logger, nameserver,
+                        node3, communication, ordering, {msg =>})
+
+    assert(communicator1.state.members === communicator2.state.members)
+    assert(communicator3.state.members === Map(node1 -> 0, node2 -> 1,
+                                               node3 -> 2))
+
+    communicator1.leaveGroup()
+    assert(communicator2.state.members === communicator3.state.members)
+    assert(communicator3.state.members === Map(node2 -> 1, node3 -> 2))
+    assert(communicator2.state.leader === node2)
+    assert(communicator3.state.leader === node2)
+
+    val node4 = new NodeID("node4", host, port)
+    val communicator4 =
+      BasicGroup.create(group, logger, nameserver,
+                        node4, communication, ordering, {msg =>})
+
+    assert(communicator2.state.members === communicator3.state.members)
+    assert(communicator3.state.members === communicator4.state.members)
+    assert(communicator4.state.members === Map(node2 -> 1, node3 -> 2,
+                                               node4 -> 3))
+    assert(communicator4.state.leader === node2)
+  }
+
+  "Counters" should "be incrementable" in {
+    val node1 = new NodeID("node1", host, port)
+    val node2 = new NodeID("node2", host, port)
+    val node3 = new NodeID("node3", host, port)
+    val communicator1 =
+      BasicGroup.create(group, logger, nameserver,
+                        node1, communication, ordering, {msg =>})
+    val communicator2 =
+      BasicGroup.create(group, logger, nameserver,
+                        node2, communication, ordering, {msg =>})
+    val communicator3 =
+      BasicGroup.create(group, logger, nameserver,
+                        node3, communication, ordering, {msg =>})
+
+    communicator1.incCounter()
+    assert(communicator2.state.msgCounter === 1)
+    assert(communicator3.state.msgCounter === 1)
+
+    communicator2.incCounter()
+    assert(communicator1.state.msgCounter === 2)
+    assert(communicator3.state.msgCounter === 2)
+
+    communicator3.incCounter()
+    assert(communicator1.state.msgCounter === 3)
+    assert(communicator2.state.msgCounter === 3)
+  }
+
+  "Groups" should "be killable" in {
+    val node1 = new NodeID("node1", host, port)
+    val node2 = new NodeID("node2", host, port)
+    val node3 = new NodeID("node3", host, port)
+    val communicator1 =
+      BasicGroup.create(group, logger, nameserver,
+                        node1, communication, ordering, {msg =>})
+    val communicator2 =
+      BasicGroup.create(group, logger, nameserver,
+                        node2, communication, ordering, {msg =>})
+    val communicator3 =
+      BasicGroup.create(group, logger, nameserver,
+                        node3, communication, ordering, {msg =>})
+
+    assert(communicator1.state.members === communicator2.state.members)
+    assert(communicator3.state.members === Map(node1 -> 0, node2 -> 1,
+                                               node3 -> 2))
+
+    communicator3.killGroup()
+    assert(communicator1.state === null)
+    assert(communicator2.state === null)
+    assert(communicator3.state === null)
+  }
+
+  "Group operations" should "be robust under non-determinism" in {
+    val node1 = new NodeID("node1", host, port)
+    val node2 = new NodeID("node2", host, port)
+    val node3 = new NodeID("node3", host, port)
+    val node4 = new NodeID("node4", host, port)
+    val node5 = new NodeID("node5", host, port)
+    val communicator1 =
+      BasicGroup.create(group, logger, nameserver,
+                        node1, communication, ordering, {msg =>})
+    var communicator2 : BasicGroup = null
+    var communicator3 : BasicGroup = null
+    var communicator4 : BasicGroup = null
+    var communicator5 : BasicGroup = null
+
+    def makeRunnable(assign : (BasicGroup => Unit), node : NodeID)
+    : Runnable = new Runnable {
+      def run() {
+        assign(BasicGroup.create(group, logger, nameserver,
+                                 node, communication, ordering, {msg =>}))
+      }
+    }
+
+    val t2 = new Thread(makeRunnable({ g => communicator2 = g }, node2))
+    val t3 = new Thread(makeRunnable({ g => communicator3 = g }, node3))
+    val t4 = new Thread(makeRunnable({ g => communicator4 = g }, node4))
+    val t5 = new Thread(makeRunnable({ g => communicator5 = g }, node5))
+    t2.start(); t3.start(); t4.start(); t5.start();
+    t2.join(); t3.join(); t4.join(); t5.join();
+
+    assert(communicator1.state.members === communicator2.state.members)
+    assert(communicator3.state.members === communicator4.state.members)
+    assert(communicator4.state.members === communicator5.state.members)
+    assert(communicator1.state.leader === node1)
+    assert(communicator2.state.leader === node1)
+    assert(communicator3.state.leader === node1)
+    assert(communicator4.state.leader === node1)
+    assert(communicator5.state.leader === node1)
+  }
+
+}
